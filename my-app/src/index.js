c7c9bc348bc6a3b74e850f2df4aba35e73514583
buglib <buglib@foxmail.com>
2023-03-13 18:12:01 +0800

    实现判断胜出者功能

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -24,6 +24,10 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -41,9 +45,16 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (
-      this.state.xIsNext ? 'X' : 'O'
-    );
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (
+        this.state.xIsNext ? 'X' : 'O'
+      );
+    }
 
     return (
       <div>
